e880dfdfa4d22735683b56bbdeb7d2543da34f0c
rc-balaji <balajic@drngpit.ac.in>
2023-12-22 00:17:33 +0530

    sss

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ const mqtt = require('mqtt');
 const cors = require('cors')
 
 const app = express();
-const port = 3000 || process.env.port;
+const port = 3000 || process.env.PORT;
 
 app.use(cors())
 // Create an MQTT client
