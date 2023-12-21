1363ec0a396f277e8e1a012ea2c6aad986adf157
rc-balaji <balajic@drngpit.ac.in>
2023-12-22 00:07:58 +0530

    first commit

---
+++ Server/server.js
@@ -0,0 +1,39 @@
+// server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mqtt = require('mqtt');
+const cors = require('cors')
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+// Create an MQTT client
+const mqttClient = mqtt.connect('mqtt://broker.hivemq.com');
+
+// Middleware to parse JSON in request body
+app.use(bodyParser.json());
+
+// Route to receive message from the React app
+app.post('/send-message', (req, res) => {
+    const dataFromReact = req.body;
+
+    // Publish the received data to MQTT
+    const message = JSON.stringify(dataFromReact);
+  // Publish the message to the MQTT broker
+  mqttClient.publish('Sami',message, (err) => {
+    if (err) {
+      console.error('Error publishing message to MQTT:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    console.log('Message published to MQTT:', message);
+    res.json({ success: true });
+  });
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+// App.jsx
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const sendMessage = async () => {
+    try {
+      await axios.post('http://localhost:3000/send-message', { message });
+      console.log('Message sent successfully!');
+    } catch (error) {
+      console.error('Error sending message:', error.message);
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <header>
+        <h1> Machine Messanger</h1>
+        <p style={{
+          margin:0,
+          marginLeft:'130px'
+        }} >-- RC Balaji</p>
+      </header>
+      <main>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Enter your message..."
+        />
+        <button onClick={sendMessage}>Send Message</button>
+      </main>
+    </div>
+  );
+};
+
+export default App;
